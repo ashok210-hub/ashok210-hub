9ed20549a754d514bd23c5cdaab15a3b4cf991f0
Ashok Kumar <ashok.k@subcodevs.com>
2025-01-26 04:28:05 +0530

    git balme function

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const age=24;
-const name="ashok kumar";
+const name1="ashok kumar";
 const bob='01/09/2003';
 
 function sayname(){
     console.log('hello ashok')
+};
+function ageAbove(){
+    console.log('hello ashok is 18 above');
 };
