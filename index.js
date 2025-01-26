3a43bbc8881e3c20388f376005ad9e27a6682574
Ashok Kumar <ashok.k@subcodevs.com>
2025-01-26 18:09:46 +0530

    multipal

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const age=24;
-const name="ashok kumar";
+const myname="ashok kumar";
 const bob='01/09/2003';
 
 function sayname(){
     console.log('hello ashok')
-<<<<<<< HEAD
 };
-function ageAbove(){
-    console.log('hello ashok is 18 above');
-};
+function multiply(){
+    console.log(`${age} is ${myname}`)
+}
 
-const father='ramratan';
-=======
-};
->>>>>>> parent of 9ed2054 (git balme function)
