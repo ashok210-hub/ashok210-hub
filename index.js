fcb84a71182c175c211c362bb84e713c29508dac
Ashok Kumar <ashok.k@subcodevs.com>
2025-01-26 17:23:33 +0530

    above function

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const age=24;
-const name1="ashok kumar";
+const name="ashok kumar";
 const bob='01/09/2003';
 
 function sayname(){
     console.log('hello ashok')
+<<<<<<< HEAD
 };
 function ageAbove(){
     console.log('hello ashok is 18 above');
 };
 
 const father='ramratan';
+=======
+};
+>>>>>>> parent of 9ed2054 (git balme function)
